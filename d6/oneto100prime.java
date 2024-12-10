4f547c4c77c35b942778564d52465127f2ea7b21
abcd-dell <pitlanagaraju572@gmail.com>
2024-12-10 22:33:55 +0530

    Add files via upload

---
+++ d6/oneto100prime.java
@@ -0,0 +1,20 @@
+public class oneto100prime {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:");
+
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false; 
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
